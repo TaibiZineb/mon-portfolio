b0a3330da9d2dd68bbb39cfc8bd24d82887d716e
Taibi Zineb <taibizineb96@gmail.com>
2026-04-01 15:44:07 +0100

    Responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,14 +5,14 @@ export default function Contact() {
   return (
     <section id="contact" className="bg-orange-50 py-20 text-center from-white px-6">
       <h2 className="text-3xl font-bold text-orange-600 py-5 ">Contact</h2>
-        <div className="bg-white shadow-xl rounded-3xl p-10 text-center leading-relaxed text-gray-700">
+        <div className="bg-white shadow-xl rounded-3xl p-10 text-center leading-relaxed text-gray-700 flex items-center gap-4 justify-center flex-col">
           <p className="mt-4 flex items-center justify-center gap-2"><Mail size={20} className="text-orange-500" /><span className="font-medium">Email :</span>
           <a href="mailto:taibizineb96@gmail.com" className="text-orange-500 hover:underline">taibizineb96@gmail.com</a></p>
           <p className="mt-4 flex items-center justify-center gap-2"><PhoneCall size={20} className="text-orange-500" /><span className="font-medium">Téléphone :</span>
           <a href="tel:+212652841972" className="text-orange-500 hover:underline">+212 6 52 84 19 72</a></p>
 
       {/* Lien LinkedIn qui s’ouvre dans un nouvel onglet */}
-      <p className="mt-4 flex items-center justify-center gap-2"><LinkedinIcon size={20} className="text-orange-500" /><span className="font-medium">LinkedIn :</span>
+      <p className="mt-4 flex items-center justify-center gap-2 flex items-center gap-4"><LinkedinIcon size={20} className="text-orange-500" /><span className="font-medium">LinkedIn :</span>
         <a
           href="https://www.linkedin.com/in/zineb-taibi"
           target="_blank"
@@ -24,7 +24,7 @@ export default function Contact() {
       </p>
 
       
-      <p className="mt-4 flex items-center justify-center gap-2"><GithubIcon size={20} className="text-orange-500" /><span className="font-medium">GitHub :</span>
+      <p className="mt-4 flex items-center justify-center gap-2 flex items-center gap-4"><GithubIcon size={20} className="text-orange-500" /><span className="font-medium">GitHub :</span>
         
         <a
           href="https://github.com/TaibiZineb"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { Code, Database, ShieldCheck, Layers, Monitor,Hand} from "lucide-react";
 export default function Home() {
   return (
-  <section id="home" className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-orange-100" >
-  <h1 className="text-4xl md:text-6xl font-bold flex items-center gap-3 animate-fadeIn"><Hand size={35}className="text-orange-500" />Bonjour, je suis <span className="text-orange-500">TAIBI Zineb</span></h1>
+  <section id="home" className="min-h-screen flex flex-col justify-center items-center text-center px-6 py-20 bg-orange-100" >
+ <h1 className="text-4xl md:text-6xl font-bold flex flex-col md:flex-row items-center md:items-start gap-3 animate-fadeIn text-center md:text-left"><Hand size={35}className="text-orange-500" />Bonjour, je suis <span className="text-orange-500">TAIBI Zineb</span></h1>
+
 
   <p className="mt-8 text-lg md:text-2xl font-medium text-gray-700 flex flex-wrap justify-center gap-6">
     <span className="flex items-center gap-2"><Layers size={20} className="text-orange-500" /> Fullstack</span>
