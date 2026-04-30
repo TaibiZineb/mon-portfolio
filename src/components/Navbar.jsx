feb0c6d6a22e0a7e5c6db4cba4b1904dad716515
Taibi Zineb <taibizineb96@gmail.com>
2026-04-30 13:05:58 +0100

    Correction de la couleur du bouton hamburger pour le mode sombre

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -110,12 +110,37 @@ export default function Navbar() {
             />
           </div>
 
-          <button
-            className="md:hidden text-white focus:outline-none"
-            onClick={() => setIsOpen(!isOpen)}
+          {/* Bouton hamburger (mobile) */}
+        <button
+          className="md:hidden text-white dark:text-black focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {/* Icône hamburger */}
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            ☰
-          </button>
+            {isOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            )}
+          </svg>
+        </button>
+
         </div>
       </div>
 
