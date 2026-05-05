f3ba281fe50e42fc8e26a1fe4455a9dea5076a2e
Taibi Zineb <taibizineb96@gmail.com>
2026-05-05 11:46:06 +0100

    Amélioration contact responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import { GithubIcon, LinkedinIcon, Mail, PhoneCall } from "lucide-react";
+import { CheckCircle, GithubIcon, LinkedinIcon, Mail, PhoneCall,  Sparkles } from "lucide-react";
 
 export default function Contact() {
   return (
@@ -7,61 +7,47 @@ export default function Contact() {
         Contact
       </h2>
 
-      <div className="max-w-3xl mx-auto bg-gradient-to-br from-white to-purple-100 shadow-xl rounded-3xl p-8 space-y-6 text-gray-700">
-
+      <div className="pace-y-4 max-w-3xl mx-auto bg-gradient-to-br from-white to-purple-100 shadow-xl rounded-3xl p-8 space-y-6 text-gray-700">
+        
         {/* Email */}
-        <div className="flex items-center gap-4 hover:translate-x-1 transition">
-          <Mail size={20} className="text-[var(--accent)]" />
-          <span className="font-semibold min-w-[100px]">Email :</span>
-          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=taibizineb96@gmail.com"
-          target="_blank" rel="noopener noreferrer" className="text-[var(--accent)] hover:underline"> taibizineb96@gmail.com</a>
+        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4 hover:translate-x-1 transition">
+          <div className="flex items-center gap-2 min-w-[140px]">
+            <Mail size={20} className="text-[var(--accent)]" />
+            <span className="font-semibold">Email :</span>
+          </div>
+
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=taibizineb96@gmail.com" target="_blank" 
+          className="text-[var(--accent)] break-all hover:underline"> taibizineb96@gmail.com</a>
         </div>
         {/* Téléphone */}
-        <div className="flex items-center gap-4 hover:translate-x-1 transition">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4 hover:translate-x-1 transition">
           <PhoneCall size={20} className="text-[var(--accent)]" />
           <span className="font-semibold min-w-[100px]">Téléphone :</span>
-          <a
-            href="tel:+212652841972"
-            className="text-[var(--accent)] hover:underline"
-          >
-            +212 6 52 84 19 72
+          <a href="tel:+212652841972" className="text-[var(--accent)] hover:underline" > +212 6 52 84 19 72
           </a>
         </div>
-
         {/* LinkedIn */}
-        <div className="flex items-center gap-4 hover:translate-x-1 transition">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4 hover:translate-x-1 transition">
           <LinkedinIcon size={20} className="text-[var(--accent)]" />
           <span className="font-semibold min-w-[100px]">LinkedIn :</span>
-          <a
-            href="https://www.linkedin.com/in/zineb-taibi"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[var(--accent)] break-all hover:underline"
-          >
-            linkedin.com/in/zineb-taibi
+          <a href="https://www.linkedin.com/in/zineb-taibi" target="_blank" rel="noopener noreferrer"
+            className="text-[var(--accent)] break-all hover:underline">linkedin.com/in/zineb-taibi
           </a>
         </div>
-
         {/* GitHub */}
-        <div className="flex items-center gap-4 hover:translate-x-1 transition">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4 hover:translate-x-1 transition">
           <GithubIcon size={20} className="text-[var(--accent)]" />
           <span className="font-semibold min-w-[100px]">GitHub :</span>
-          <a
-            href="https://github.com/TaibiZineb"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[var(--accent)] hover:underline"
-          >
-            Voir mes projets
+          <a href="https://github.com/TaibiZineb"
+            target="_blank" rel="noopener noreferrer"
+            className="text-[var(--accent)] hover:underline"> Voir mes projets
           </a>
         </div>
-
         {/* CTA */}
-        <div className="pt-4 text-center">
-          <p className="text-sm text-[var(--accent)]">
-            Disponible pour opportunités 
-          </p>
-        </div>
+       <div className="pt-4 flex justify-center items-center gap-2 text-[var(--accent)]">
+        <Sparkles size={18} className="animate-pulse" />
+        <p className="text-sm font-medium"> Disponible pour opportunités</p>
+      </div>
 
       </div>
     </section>
